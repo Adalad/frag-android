430d14a261b1c4787ca4de481b4d8d0ee591d7ee
alguilleal@gmail.com <alguilleal@gmail.com@a1ed0f45-41ca-ecac-7955-ea857cf40fef>
2010-12-09 23:39:08 +0000

    opciones de menu y retocar el popup

--- FRAGUEL/src/fraguel/android/states/ImageState.java
+++ FRAGUEL/src/fraguel/android/states/ImageState.java
@@ -10,33 +10,33 @@ import fraguel.android.State;
 import fraguel.android.gallery.ImageAdapter;
 
 public class ImageState extends State{
-	
+
 	public ImageState() {
 		super();
 		id = 4;
 	}
-	
-	
+
+
 	@Override
 	public void load() {
 		// TODO Auto-generated method stub
 		viewGroup = new Gallery(FRAGUEL.getInstance().getApplicationContext());
 		((Gallery) viewGroup).setAdapter(new ImageAdapter(FRAGUEL.getInstance().getApplicationContext()));
 
-	    ((Gallery)viewGroup).setOnItemClickListener(new OnItemClickListener() {
-	        public void onItemClick(AdapterView parent, View v, int position, long id) {
-	            Toast.makeText(FRAGUEL.getInstance().getApplicationContext(), "" + position, Toast.LENGTH_SHORT).show();
-	        }
-	    });
+		((Gallery)viewGroup).setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView parent, View v, int position, long id) {
+				Toast.makeText(FRAGUEL.getInstance().getApplicationContext(), "" + position, Toast.LENGTH_SHORT).show();
+			}
+		});
 
-	    FRAGUEL.getInstance().addView(viewGroup);
-		
+		FRAGUEL.getInstance().addView(viewGroup);
+
 	}
 
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
