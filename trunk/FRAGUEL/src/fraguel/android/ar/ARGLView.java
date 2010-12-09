da6c53d3ec2f0e8d1be5498d2fd98c02a51a6c50
gpr1985 <gpr1985@a1ed0f45-41ca-ecac-7955-ea857cf40fef>
2010-12-09 12:12:29 +0000

    principios de realidad aumentada

---
+++ trunk/FRAGUEL/src/fraguel/android/ar/ARGLView.java
@@ -0,0 +1,31 @@
+package fraguel.android.ar;
+
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.opengl.GLSurfaceView;
+
+public class ARGLView extends GLSurfaceView {
+
+	/** ATTRIBUTES **/
+	// Renderer
+	private ARRenderer _renderer;
+
+	// World position (GPS coordinates)
+	// private float[] _worldPosition = { 0f, 0f, 0f };
+	// World orientation (degrees from north
+	// private int[] _worldRotation = { 0, 0, 0 };
+
+	/** CONSTRUCTOR **/
+	public ARGLView(Context context) {
+		super(context);
+
+		_renderer = new ARRenderer();
+		setRenderer(_renderer);
+		//setEGLConfigChooser(8, 8, 8, 8, 16, 0);
+		//getHolder().setFormat(PixelFormat.TRANSLUCENT);
+	}
+
+	/** PUBLIC METHODS **/
+	// TODO procesar eventos, GPS, brujula, acelerometro
+
+}
