c856dfc93011db24a82706c4fda35771635a4aef
gpr1985 <gpr1985@a1ed0f45-41ca-ecac-7955-ea857cf40fef>
2010-12-21 11:14:55 +0000

    Ya lee las rutas y los puntos de interes, esto sirve para que se puedan leer puntos y luego asociarlos a una ruta (en dicho modo) o dejarlos tal cual (en modo libre). Falta puentearlo con los mapas.

---
+++ trunk/FRAGUEL/src/fraguel/android/xml/PointsHandler.java
@@ -0,0 +1,57 @@
+package fraguel.android.xml;
+
+import java.util.ArrayList;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class PointsHandler extends DefaultHandler {
+
+	private boolean in_titletag;
+
+	private ArrayList<PointOI> points;
+	private PointOI currentPoint;
+
+	public void endDocument() throws SAXException {
+	}
+
+	public void startDocument() throws SAXException {
+		in_titletag = false;
+
+		points = new ArrayList<PointOI>();
+	}
+
+	public void characters(char[] ch, int start, int length)
+			throws SAXException {
+		if (in_titletag) {
+			currentPoint.title = new String(ch, start, length);
+		}
+	}
+
+	public void startElement(String uri, String localName, String qName,
+			Attributes attributes) throws SAXException {
+		if (localName.equals("point")) {
+			currentPoint = new PointOI();
+			currentPoint.id = Integer.parseInt(attributes.getValue("id"));
+		} else if (localName.equals("coords")) {
+			currentPoint.coords[0] = Float.parseFloat(attributes.getValue("x"));
+			currentPoint.coords[1] = Float.parseFloat(attributes.getValue("y"));
+		} else if (localName.equals("title")) {
+			in_titletag = true;
+		}
+	}
+
+	public void endElement(String uri, String localName, String qName)
+			throws SAXException {
+		if (localName.equals("point")) {
+			points.add(currentPoint);
+		} else if (localName.equals("title")) {
+			in_titletag = false;
+		}
+	}
+
+	public ArrayList<PointOI> getParsedData() {
+		return points;
+	}
+}
